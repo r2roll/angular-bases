de7706edfe196648f5f7117a43e9ad6de4cda299
r2roll <arturollamasruiz@gmail.com>
2023-07-30 10:06:27 -0700

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+        <h3>Counter:{{counter}}</h3>
+        <button (click)="increaseBy(+1)">+1</button>
+        <button (click)="resetCounter()">Reset</button>
+        <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  constructor() { }
+
+    public title:string = 'Hola mundo';
+    public counter:number=10;
+
+    increaseBy(value:number):void{
+      this.counter+=value;
+    }
+
+    resetCounter():void{
+      this.counter=10;
+    }
+
+
+}
+
+
+
+// import {Component} from '@angular/core'
+
+// @Component({
+//   template:'<h1>Hola Counter</h1>',
+//   selector:'app-counter'
+// })
+// export class CounterComponnet
+// {
+
+// }
